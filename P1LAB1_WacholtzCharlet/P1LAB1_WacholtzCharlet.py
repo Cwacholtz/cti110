8cc55e74e8b0b899c69da02cd88166a3243575e8
Cwacholtz <cwacholtz1@gmail.com>
2024-06-20 13:20:08 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_WacholtzCharlet/P1LAB1_WacholtzCharlet.py
@@ -0,0 +1,10 @@
+# Charlet Wacholtz
+# 06/18/2024
+# P1LAB1
+# A program that displays a message welcoming the user to the course.
+
+first_name = input ("Enter your first name:")
+
+last_name = input ("Enter your last name:")
+
+print ("Hello",first_name, last_name + "!", "Welcome to CTI-110")
